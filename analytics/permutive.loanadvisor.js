6baa1dc2542fd7afb2bfa8d21b40480e78f46ce7
joey <joea@undergroundelephant.com>
2020-04-15 16:07:05 -0700

    Load adivisor scripts

---
+++ analytics/permutive.loanadvisor.js
@@ -0,0 +1,102 @@
+/* W4 Permutive Helper Script */
+
+/**
+ * Force value to string.
+ * @param answer
+ * @returns string
+ */
+function forceString(answer) {
+    if (typeof answer == "string") {
+        return answer;
+    }
+
+    if (typeof answer.toString == "function") {
+        return answer.toString();
+    }
+
+    return "" + answer;
+}
+
+/**
+ * Extract URL Parameters.
+ * @param namespace
+ * @returns {string|null}
+ */
+function grabUrlArg(namespace) {
+    namespace = namespace.toLowerCase();
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key.toLowerCase()] = value;
+    });
+
+    if (vars.hasOwnProperty(namespace)) {
+        if (vars[namespace].indexOf('#')) {
+            vars[namespace] = vars[namespace].split('#')[0];
+        }
+
+        return vars[namespace] ? vars[namespace] : "N/A";
+    }
+
+    return null;
+}
+
+/**
+ * Tracking using permutive script
+ * @param postQuestion
+ * @param postAnswer
+ */
+
+function doTrack(postQuestion, postAnswer) {
+    if (typeof window.permutive !== 'undefined') {
+        window.permutive.track('Submit', {
+            "client": {
+                "url": document.location.href,
+                "domain": document.location.hostname,
+                "referrer": document.referrer,
+                "title": document.title,
+                "type": "web",
+                "user_agent": navigator.userAgent,
+            },
+            "form": {
+                "answer": forceString(postAnswer),
+                "question": postQuestion,
+            },
+            "network": {
+                "campaignId": grabUrlArg('utm_campaign'),
+                "pubId": grabUrlArg('sub2'),
+                "source": grabUrlArg('utm_source'),
+                "subId": grabUrlArg('sub1')
+            }
+        });
+    }
+}
+
+
+/* BIND TO :input Change Events */
+(function($) {
+    $(function () {
+        $(document).on( 'change', ':input', function() {
+            var postQuestion = $(this).attr('name');
+            var postAnswer   = $(this).val();
+
+            if (typeof window.queryDmap !== 'undefined') {
+                var dmapSet = queryDmap(postQuestion, postAnswer);
+                postQuestion = dmapSet[0];
+                postAnswer = dmapSet[1];
+                doTrack(postQuestion, postAnswer);
+            }
+        });
+
+        $(document).on( 'click', ':input', function() {
+            var postQuestion = $(this).attr('name');
+            var postAnswer   = $(this).val();
+
+            if (typeof window.queryDmap !== 'undefined') {
+                var dmapSet = queryDmap(postQuestion, postAnswer);
+                postQuestion = dmapSet[0];
+                postAnswer = dmapSet[1];
+                doTrack(postQuestion, postAnswer);
+            }
+        });
+    });
+})(jQuery);
